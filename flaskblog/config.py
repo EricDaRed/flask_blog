0446313d687a14c2a47b286f175f9a9add89486a
Eric <ekernal@gmail.com>
2020-06-25 10:13:52 -0700

    Exlcluding config from git

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,6 +4,8 @@ import json
 with open('/etc/flask_blog_config.json') as config_file:
 	config = json.load(config_file)
 
+#updated after excluding
+
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
